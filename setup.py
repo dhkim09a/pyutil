d70ee351bc8d78c7462a6e863754d783c0092801
Donghwi Kim <dhkim09a@gmail.com>
2020-10-21 23:09:45 +0900

    v0.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="yautil", # Replace with your own username
-    version="0.0.5",
+    version="0.0.6",
     author="Donghwi Kim",
     author_email="dhkim09@kaist.ac.kr",
     description="Yet Another Python util.",
